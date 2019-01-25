c8a3ca5d3f2379af812bc39d3cdadc06e90b863d
gosho-kazuya <ketsume0211@gmail.com>
2019-01-24 22:20:55 -0800

    create logic!

---
+++ schema-rb-to-graphq.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'bundler'
+require 'active_support/inflector'
+
+def convert(type)
+  type
+    .sub('string', 'String')
+    .sub('int', 'Int')
+    .sub('bigint', 'Int')
+    .sub('boolean', 'Boolean')
+end
+
+class T
+  def method_missing(method_name, *args, &block)
+    return nil if method_name == :index
+
+    puts "  #{args[0]}: #{convert(method_name.to_s)}"
+  end
+end
+
+class Object
+  def enable_extension(*args)
+  end
+
+  def create_table(table_name, *args, &block)
+    puts "type #{table_name.classify} {"
+    block.call(T.new)
+    puts "}"
+    puts ""
+  end
+end
+
+module ActiveRecord
+  module Schema
+    def self.define(args, &block)
+      puts block.call
+    end
+  end
+end
+
+require './schema.rb'
